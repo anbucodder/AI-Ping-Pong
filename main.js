ff110a7036e3295d1547d521cf534ea3582f2bf8
anbucodder <100592337+anbucodder@users.noreply.github.com>
2022-12-11 09:46:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 function preload() {
     back = loadImage("ping-pong.jpg");
+    ball  = loadImage("ball.png");
 }
 
 function setup() {
-	canvas = createCanvas(900,500);
+	canvas = createCanvas(700,400);
+    canvas.parent("canvas");
     canvas.center();
-}
 
+    video = createCapture(VIDEO);
+	video.size(375,1100);
+	video.position(550,330);
+	video.parent("game_console");
+
+	poseNet= ml5.poseNet(video , modalloaded);
+	poseNet.on("pose" , gotposes);
+}
+   function	modalloaded(){
+	console.log("Modal loaded");
+}
+function gotposes(results){
+    if(results.length > 0){
+       rightwristy = results[0].pose.rightWrist.y;
+       console.log(results);
+       console.log("Right wrist y = "+rightwristy);
+    }
+}
 function draw() {
-	image(back,0,0,900,500);
+	image(back,0,0,700,400);
 }
